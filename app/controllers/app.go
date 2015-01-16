9d1e8a00aa4d2e316cf94d33e3ac096d5d6b4677
uzimith <uzimith.x9@gmail.com>
2015-01-16 20:41:43 +0900

    add App.Room

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,49 +2,19 @@ package controllers
 
 import (
 	"github.com/revel/revel"
-	"github.com/uzimith/outcastify/app/room"
+	"github.com/uzimith/outcastify/app/helper"
 )
-
-import "code.google.com/p/go.net/websocket"
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	id := helper.GenerateRandom(16)
+	return c.Render(id)
 }
 
-func (c App) Socket(user string, ws *websocket.Conn) revel.Result {
-	revel.INFO.Printf("new connection:" + user)
-	revel.INFO.Printf("%+v", ws)
-	event := room.NewEvent("join", "me", "I got")
-	if websocket.JSON.Send(ws, event) != nil {
-		return nil
-	}
-	newMessages := make(chan string)
-	go func() {
-		var msg string
-		for {
-			err := websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				close(newMessages)
-				return
-			}
-			newMessages <- msg
-		}
-	}()
-
-	for {
-		select {
-		case msg, ok := <-newMessages:
-			if !ok {
-				return nil
-			}
-
-			revel.INFO.Println(msg)
-			// room.Say(user, msg)
-		}
-	}
-	return nil
+func (c App) Room(id string) revel.Result {
+	user := helper.GenerateRandom(16)
+	return c.Render(id, user)
 }
